389ddbcdec91937ffcca839e0e51cfc3b40deb72
anktony <antoneslima@gmail.com>
2022-04-06 22:47:13 -0300

    minor changes

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,24 +1,18 @@
 import React from "react";
 import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 import './MapPage.css'
-import MapStarter from "../components/MapStarter.js"
+import MapStarter from "../local/MapStarter.js"
 
 
+const mapLoader = MapStarter();
 
-// export interface MapPageProps{
-// }
+
 
 const MapPage = () => {
 
-
-    const mapLoader = MapStarter();
-    const position_penedo =  { lat: -10.2800, lng: -36.5400}
-
-
-    
     const {isLoaded} = useJsApiLoader({
          id: mapLoader.id,
-         googleMapsApiKey: mapLoader.googleMapsApiKey,
+         googleMapsApiKey: mapLoader.key,
      })
   
     return  (
@@ -27,18 +21,9 @@ const MapPage = () => {
                 isLoaded? (
                     <GoogleMap
                         mapContainerStyle={{width: '100%', height: '100%'}}
-                        center = {position_penedo}
-                        zoom = {15}>
+                        center = {mapLoader.position}
+                        zoom = {mapLoader.zoom}>
 
-                        <Marker
-                            position={position_penedo} 
-                            options={{
-                                label: {
-                                    text: "Posição Teste (center)",
-                                    className: "map-marker"
-                                },
-                            }}
-                        />
                     </GoogleMap>
                 ):(<></>)
             }
